26506482feea69ddce0ee431d59d6e64a521b3a0
solankiji28 <viveksolanki7772@gamil.com>
2024-11-29 16:46:21 +0530

    update files

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Navigation from "./src/navigation/Navigation";
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
+import { enableScreens } from "react-native-screens";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+enableScreens();
 
 const queryClient = new QueryClient();
 
